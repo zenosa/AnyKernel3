ff3df54cafda4b4a594903cdee67e118473adcb8
zenosa <95568589+zenosa@users.noreply.github.com>
2022-08-13 14:52:44 +0800

    anykernel3: Add support for Kenzo

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Redmi 5 Plus kernel
+kernel.string=Lionus-v4.9-pie
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=vince
+device.name1=kenzo
 supported.versions=
 supported.patchlevels=
 '; } # end properties
